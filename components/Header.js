29596f457e2db3943379925dfbaf73db66b8f15b
vpess <vpess@protonmail.com>
2021-12-31 00:59:48 -0300

    Alteração de texto do header.

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
         <header>
         <div className='container'>
             <Link href='/' passHref>
-                <h2>Anotações™</h2>
+                <h2>✍️ Anotações™</h2>
             </Link>
         </div>
         </header>
